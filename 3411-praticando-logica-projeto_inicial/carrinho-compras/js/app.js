240b1390261ee36de4d2cbc12b24f61109ae204f
Dierlen Hallze Alexandre <dierlen84@gmail.com>
2024-06-19 18:41:53 -0300

    Desafio 03 Feito

--- 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,16 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    limpar();
+});
 
-
-function adicionar(){
+function adicionar() {
     let prodvalue = document.getElementById("produto").value;
     let produto = prodvalue.split(' - ')[0];
-    let quantidade = document.getElementById("quantidade").value;
-    let valor = prodvalue.split(' - ')[1];
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let valorUnitario = parseInt(prodvalue.split(' - R$')[1]);
+    let valorComprado = valorUnitario * quantidade;
 
-    let carrinho= document.querySelector(".carrinho__produtos__produto");
+    let carrinho = document.getElementById("lista-produtos");
+    let produtoExistente = Array.from(carrinho.children).find(p => p.dataset.produto === produto);
 
-    let novoprod = `<span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">${valor}</span>`
+    if (produtoExistente) {
+        let quantElem = produtoExistente.querySelector('.quantidade');
+        let valorElem = produtoExistente.querySelector('.valor');
+        let novaQuantidade = parseInt(quantElem.textContent.split('x')[0]) + quantidade;
+        quantElem.textContent = novaQuantidade + 'x';
+        valorElem.textContent = `R$${(novaQuantidade * valorUnitario).toLocaleString('pt-BR')}`;
+    } else {
+        let novoProduto = `<section class="carrinho__produtos__produto" data-produto="${produto}">
+            <span class="texto-azul quantidade">${quantidade}x</span> ${produto} 
+            <span class="texto-azul valor">R$${valorComprado.toLocaleString('pt-BR')}</span>
+        </section>`;
+        carrinho.insertAdjacentHTML('beforeend', novoProduto);
+    }
 
-    carrinho.append(`<span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">${valor}</span>`)
+    atualizarTotal(valorComprado);
+}
 
-    console.log(carrinho)
+function atualizarTotal(valorAdicionado) {
+    let valorTotalElemento = document.getElementById("valor-total");
+    let valorTotalAtual = parseInt(valorTotalElemento.textContent.replace('R$', '').replace(/\./g, '').replace(',', '.')) || 0;
+    let novoValorTotal = valorTotalAtual + valorAdicionado;
+    valorTotalElemento.textContent = `R$${novoValorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+}
+
+function limpar() {
+    let carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML = '';
+    document.getElementById("valor-total").textContent = 'R$0,00';
 }
