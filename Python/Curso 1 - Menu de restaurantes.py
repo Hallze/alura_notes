aed5b66df84b73a87bf6fa0672053e3b198942d8
Dierlen Hallze Alexandre <dierlen84@gmail.com>
2024-06-27 07:47:29 -0300

    Avanço para modulo de python

---
+++ Python/Curso 1 - Menu de restaurantes.py
@@ -0,0 +1,9 @@
+print('\nBuggio Eco\n') 
+
+print('1. Cadastrar Restaurantes') 
+print('2. Listar Eco') 
+print('3. Ativar Eco') 
+print('4. Sair\n') 
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Voce escolheu a opção:{opcao_escolhida}\n') 
